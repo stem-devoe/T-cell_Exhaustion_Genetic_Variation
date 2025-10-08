a298966fa396adf5643d8fad25175bc042e24fb4
Stephanie DeVoe <stemdevoe@gmail.com>
2025-10-08 10:07:18 -0400

    Delete NOD_StructualVariation directory
    deleting to re-upload with file name changes

--- NOD_StructualVariation/nfcore_parameters/run_DeNovo_nfcore_atacseq.sh
+++
@@ -1,8 +0,0 @@
-nextflow run nf-core/atacseq -profile docker \
-        -r 2.1.2 \
-        -c /Scottbrowne/members/smd/Projects/SD032/nfcore_parameters/atacseq_custom.config \
-        -params-file /Scottbrowne/members/smd/Projects/SD032/nfcore_parameters/nfcore_atacseq.yaml \
-        --input /Scottbrowne/members/smd/Projects/SD032/nfcore_parameters/nfcore_atacseq-input.csv \
-        --outdir /Scottbrowne/seq/tmp/devoes/SD032/nfcore_outs/GCA_921998325.2_NOD_ShiLtJ_v3_genomic \
-		--max_cpus 24 \
-		--max_memory 160.GB
